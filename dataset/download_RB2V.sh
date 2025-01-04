0b064c81116b7a74efc955a42a93e90d974568fb
Phạm Băng Đăng <bangdang2000@gmail.com>
2025-01-04 20:47:20 +0700

    Update download_RB2V.sh

--- dataset/download_RB2V.sh
+++ dataset/download_RB2V.sh
@@ -4,9 +4,9 @@
 cd dataset
 
 # Download the datasets
-wget https://public.vinai.io/RB2Vaa
-wget https://public.vinai.io/RB2Vab
-wget https://public.vinai.io/RB2Vac
+wget https://huggingface.co/datasets/anhttran1111/rb2v/resolve/main/RB2Vaa
+wget https://huggingface.co/datasets/anhttran1111/rb2v/resolve/main/RB2Vab
+wget https://huggingface.co/datasets/anhttran1111/rb2v/resolve/main/RB2Vac
 
 # Combine the files into a zip file
 cat RB2Vaa RB2Vab RB2Vac > RB2V.zip
